ec7f3c98b74bdde96638d980d2968580f7efcfb9
Kenji Saito <ken-yo@mbr.nifty.com>
2021-06-24 19:17:09 +0900

    Add auto update

--- src/client.ts
+++ src/client.ts
@@ -1,65 +1,66 @@
 import { graphql } from "@octokit/graphql";
 
 interface FindPullRequestIdParam {
-    owner: string;
-    repo: string;
-    number: number;
+  owner: string;
+  repo: string;
+  number: number;
 }
 
 interface IGitHubClient {
-    findPullRequestId(params: FindPullRequestIdParam): Promise<string | undefined>;
-    enableAutoMerge(pullRequestId: string): Promise<void>;
+  findPullRequestId(params: FindPullRequestIdParam): Promise<string | undefined>;
+  enableAutoMerge(pullRequestId: string): Promise<void>;
 }
 
 class GitHubClient implements IGitHubClient {
-    private token: string;
+  private token: string;
 
-    constructor(token: string) {
-        this.token = token;
+  constructor(token: string) {
+    this.token = token;
+  }
+  async findPullRequestId(params: FindPullRequestIdParam): Promise<string | undefined> {
+    const query = `
+    query {
+        repository(owner: ${params.owner}, name: ${params.repo}) {
+          pullRequest(number: ${params.number}) {
+            node {
+              id
+            }
+          }
+        }
     }
-    async findPullRequestId(params: FindPullRequestIdParam): Promise<string | undefined> {
-        const { data } = await graphql(
-            `
-            query {
-                repository(owner: ${params.owner}, name: ${params.repo}) {
-                  pullRequest(number:${params.number}) {
-                    node {
-                      id
-                    }
-                  }
-                }
-            }
-            `,
-            {
-              headers: {
-                authorization: `token ${this.token}`,
-              },
-            }
-          );
-          return data.repository?.pullRequest?.id;
-    }
-    async enableAutoMerge(pullRequestId: string): Promise<void> {
-        await graphql(
-            `
-            mutation {
-                enablePullRequestAutoMerge(input: {
-                    pullRequestId: "${pullRequestId}",
-                    clientMutationId : null
-                  }) {
-                    clientMutationId
-                  }
-                }
-            }
-            `,
-            {
-              headers: {
-                authorization: `token ${this.token}`,
-              },
-            }
-          );
-    }
+    `;
+    const { data } = await graphql(
+      query,
+      {
+        headers: {
+          authorization: `token ${this.token}`,
+        },
+      }
+    );
+    return data.repository !== undefined && data.repository.pullRequest !== undefined ? data.repository.pullRequest.id : undefined;
+  }
 
-    
+  async enableAutoMerge(pullRequestId: string): Promise<void> {
+    const query = `
+      mutation {
+          enablePullRequestAutoMerge(input: {
+              pullRequestId: "${pullRequestId}",
+              clientMutationId : null
+            }) {
+              clientMutationId
+            }
+          }
+      }
+      `;
+    await graphql(
+      query,
+      {
+        headers: {
+          authorization: `token ${this.token}`,
+        },
+      }
+    );
+  }
 }
 
 export default GitHubClient;
