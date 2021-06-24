e77ac36212c8cc5b40c6559abe1cdf8ccb66d399
Kenji Saito <ken-yo@mbr.nifty.com>
2021-06-24 19:17:09 +0900

    Add auto update

--- dist/client.js
+++ dist/client.js
@@ -7,35 +7,37 @@ class GitHubClient {
         this.token = token;
     }
     async findPullRequestId(params) {
-        const { data } = await graphql_1.graphql(`
-            query {
-                repository(owner: ${params.owner}, name: ${params.repo}) {
-                  pullRequest(number:${params.number}) {
-                    node {
-                      id
-                    }
-                  }
-                }
+        const query = `
+    query {
+        repository(owner: ${params.owner}, name: ${params.repo}) {
+          pullRequest(number: ${params.number}) {
+            node {
+              id
             }
-            `, {
+          }
+        }
+    }
+    `;
+        const { data } = await graphql_1.graphql(query, {
             headers: {
                 authorization: `token ${this.token}`,
             },
         });
-        return data.repository?.pullRequest?.id;
+        return data.repository !== undefined && data.repository.pullRequest !== undefined ? data.repository.pullRequest.id : undefined;
     }
     async enableAutoMerge(pullRequestId) {
-        await graphql_1.graphql(`
-            mutation {
-                enablePullRequestAutoMerge(input: {
-                    pullRequestId: "${pullRequestId}",
-                    clientMutationId : null
-                  }) {
-                    clientMutationId
-                  }
-                }
+        const query = `
+      mutation {
+          enablePullRequestAutoMerge(input: {
+              pullRequestId: "${pullRequestId}",
+              clientMutationId : null
+            }) {
+              clientMutationId
             }
-            `, {
+          }
+      }
+      `;
+        await graphql_1.graphql(query, {
             headers: {
                 authorization: `token ${this.token}`,
             },
