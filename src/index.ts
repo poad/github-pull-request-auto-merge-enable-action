77d536ca1ed92dea57debdd6e46166668fc68ffc
Kenji Saito <ken-yo@mbr.nifty.com>
2024-04-30 14:31:01 +0900

    Add the approve function

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import * as core from "@actions/core";
+import { GraphqlResponseError } from "@octokit/graphql";
+import "source-map-support/register";
+import { run } from "./main";
+
+// error handler
+function errHandler(error: unknown) {
+  if (error instanceof GraphqlResponseError) {
+    core.error(
+      'Unable to enable automerge.Enable branch protection and activate one or more "branch protection rules". See https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/incorporating-changes-from-a-pull-request/automatically-merging-a-pull-request#enabling-auto-merge and https://docs.github.com/en/repositories/configuring-branches-and-merges-in-your-repository/managing-protected-branches/managing-a-branch-protection-rule for more information',
+    );
+    core.setFailed(error);
+    return;
+  }
+  if (error instanceof Error) {
+    core.error(error);
+    core.setFailed(error);
+    return;
+  }
+  const e = error instanceof Error ? error : JSON.stringify(error);
+  core.error(e);
+  core.setFailed(e);
+}
+
+/// main process
+async function main(): Promise<void> {
+  try {
+    const token: string = core.getInput("github_token");
+    const pullRequestNumber = Number(core.getInput("pull_request_number"));
+    const owner: string = core.getInput("owner");
+    const repo: string = core.getInput("repository");
+    const mergeMethod: string = core.getInput("merge_method");
+    const withApprove: boolean = core.getBooleanInput("with_approve");
+
+    core.info(`owner: ${owner}`);
+    core.info(`repository: ${repo}`);
+    core.info(`pull_request_number: ${pullRequestNumber}`);
+    core.info(`merge_method: ${mergeMethod}`);
+
+    if (pullRequestNumber === 0) {
+      errHandler(
+        new Error("pull_request_number or pull_request_id must be specified"),
+      );
+    }
+
+    run({
+      token,
+      pullRequestNumber,
+      owner,
+      repo,
+      mergeMethod,
+      withApprove,
+    });
+  } catch (error) {
+    errHandler(error);
+  }
+}
+
+/// entrypoint
+function entrypoint() {
+  Promise.resolve(main()).catch((error: Error) => {
+    core.error(error.stack ? error.stack?.toString() : error);
+    core.setFailed(error);
+  });
+}
+
+entrypoint();
