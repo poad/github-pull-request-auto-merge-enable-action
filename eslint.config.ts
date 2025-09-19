69657553c87f3000785dd1a038f34b365d1e4c96
Kenji Saito <ken-yo@mbr.nifty.com>
2025-09-19 23:14:27 +0900

    CI設定・依存パッケージ・構成ファイルの更新
    - CIワークフロー（ci.yml）の修正
    - dependabot設定の更新
    - 各種lockファイル・パッケージ管理ファイル（bun.lock, package.json, pnpm-lock.yaml, pnpm-workspace.yaml）の更新
    - eslint設定ファイルの修正
    - update.shの更新

---
+++ eslint.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'eslint/config';
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+
+// @ts-expect-error: No types
+import github from 'eslint-plugin-github';
+
+import { configs } from 'typescript-eslint';
+
+export default defineConfig(
+  {
+    ignores: [
+      '**/*.d.ts',
+      '*.{js,jsx}',
+      'node_modules/**/*',
+      'dist',
+    ],
+  },
+  eslint.configs.recommended,
+  ...configs.strict,
+  ...configs.stylistic,
+  {
+    files: ['src/**/*.ts', 'app/**/*.{js,jsx,ts,tsx}'],
+    extends: [
+      ...configs.recommended,
+    ],
+    plugins: {
+      '@stylistic': stylistic,
+      github,
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/indent': ['error', 2],
+      'max-len': ['error', 80],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/quotes': ['error', 'single'],
+    },
+  },
+);
