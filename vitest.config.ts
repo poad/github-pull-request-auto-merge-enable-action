749dd1614b2fc511dbe864ac7db1ed7c23aea6f1
Kenji Saito <ken-yo@mbr.nifty.com>
2026-04-09 00:09:28 +0900

    Bumps node modules

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,9 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vitest/config';
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths({
-      projects: ['tsconfig-test.json']
-    })
-  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     coverage: {
       reporter: ['json', 'html', 'cobertura']
